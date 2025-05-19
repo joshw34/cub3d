9c67705107265f4365de2b5d7bcf09a232251907
joshw34 <159394665+joshw34@users.noreply.github.com>
2025-05-19 15:01:42 +0200

    moved init for game/ray to run_game()

--- src/game/run_game.c
+++ src/game/run_game.c
@@ -13,28 +13,26 @@
 #include "../../inc/cub3d.h"
 
 /* Initialise all images then redraw in loop after every keypress */
-void	init_map(t_data *data, t_game *game)
-{
-	mlx_put_image_to_window(data->init, data->rc_win,
-		game->game_img, 0, 0);
+void init_map(t_data *data, t_game *game) {
+  mlx_put_image_to_window(data->init, data->rc_win, game->game_img, 0, 0);
 }
 
 /* Create mlx, window and image pointers. */
-static void	init_mlx_data(t_data *data)
-{
-	data->rc_win = mlx_new_window(data->init, WINX, WINY, "cub3d");
-	data->game->bg_img = mlx_new_image(data->init, WINX, WINY);
-	data->game->game_img = mlx_new_image(data->init, WINX, WINY);
+static void init_mlx_data(t_data *data) {
+  data->rc_win = mlx_new_window(data->init, WINX, WINY, "cub3d");
+  data->game->bg_img = mlx_new_image(data->init, WINX, WINY);
+  data->game->game_img = mlx_new_image(data->init, WINX, WINY);
 }
 
-void	run_game(t_data *data)
-{
-	init_mlx_data(data);
-	set_image_data(data);
-	raycasting(data, data->ray, data->player, data->game);
-	init_map(data, data->game);
-	mlx_hook(data->rc_win, DestroyNotify, 0, &win_close, data);
-	mlx_expose_hook(data->rc_win, &expose_win, data);
-	mlx_hook(data->rc_win, KeyPress, KeyPressMask, &key, data);
-	mlx_loop(data->init);
+void run_game(t_data *data) {
+  init_game_struct(data);
+  init_ray_struct(data);
+  init_mlx_data(data);
+  set_image_data(data);
+  raycasting(data, data->ray, data->player, data->game);
+  init_map(data, data->game);
+  mlx_hook(data->rc_win, DestroyNotify, 0, &win_close, data);
+  mlx_expose_hook(data->rc_win, &expose_win, data);
+  mlx_hook(data->rc_win, KeyPress, KeyPressMask, &key, data);
+  mlx_loop(data->init);
 }
--- src/main.c
+++ src/main.c
@@ -12,31 +12,27 @@
 
 #include "../inc/cub3d.h"
 
-t_data	*init_structs(char	*map_file)
-{
-	t_data	*data;
+t_data *init_structs(char *map_file) {
+  t_data *data;
 
-	data = ft_calloc(1, sizeof(t_data));
-	if (!data)
-		exit_game(data, true, ERROR_1);
-	data->init = mlx_init();
-	init_map_struct(data, map_file);
-	init_player_struct(data);
-	init_texture_struct(data);
-	init_ray_struct(data);
-	init_game_struct(data);
-	return (data);
+  data = ft_calloc(1, sizeof(t_data));
+  if (!data)
+    exit_game(data, true, ERROR_1);
+  data->init = mlx_init();
+  init_map_struct(data, map_file);
+  init_player_struct(data);
+  init_texture_struct(data);
+  return (data);
 }
 
 /* Data split in to multiple structs to keep tidy, need to reorganise later */
-int	main(int ac, char **av)
-{
-	t_data	*data;
+int main(int ac, char **av) {
+  t_data *data;
 
-	if (ac != 2)
-		exit(EXIT_FAILURE);
-	data = init_structs(av[1]);
-	parsing(data, data->map);
-	run_game(data);
-	exit_game(data, false, NULL);
+  if (ac != 2)
+    exit(EXIT_FAILURE);
+  data = init_structs(av[1]);
+  parsing(data, data->map);
+  run_game(data);
+  exit_game(data, false, NULL);
 }
